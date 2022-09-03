4f69bcb70a687a12536a79d485d58bb877446958
Mojtaba Bavand <39924021+MBS1984@users.noreply.github.com>
2022-09-03 09:26:57 -0400

    Update open_data.py

--- open_data.py
+++ open_data.py
@@ -1,10 +1,10 @@
+    """
+    Function to open a dataset from a pickle file
+    """
 import pickle as pkl
 
 def open_data(path):
-    """
-Function to open a dataset from a .pkl file
-    """
-        
+
     with open(path, 'rb') as f:
         data = pkl.load(f)
         
