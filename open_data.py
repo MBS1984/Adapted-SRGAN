730a4c6fa805dd4bbb73005ad7e990456c0a714d
Mojtaba Bavand <39924021+MBS1984@users.noreply.github.com>
2022-09-03 09:25:27 -0400

    Update open_data.py

--- open_data.py
+++ open_data.py
@@ -2,9 +2,7 @@ import pickle as pkl
 
 def open_data(path):
     """
-    Fonction pour ouvrir un jeu de données à partir d'un fichier .pkl
-    in : path = le chemin du fichier
-    out : data - les données contenues
+Function to open a dataset from a .pkl file
     """
         
     with open(path, 'rb') as f:
